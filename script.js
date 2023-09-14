51593434308b2148514f832740ef310fac5b9d8a
PrGhostDev <139947007+PrGhostDev@users.noreply.github.com>
2023-09-14 21:29:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function decimalToBinary(num) {
   //Write you code here
+	  if (num === 0) {
+    return '0'; 
+  }
+
+  let binary = '';
+  while (num > 0) {
+    let remainder = num % 2;
+    
+    binary = remainder.toString() + binary;
+    
+    num = Math.floor(num / 2);
+  }
+
+  return binary;
   
 }
 
